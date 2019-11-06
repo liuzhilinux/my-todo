8c0c5d932f4c4eaef1b7b454c4932044955858be
ling <i@e7iling.com>
2019-11-07 01:18:05 +0800

    修改 java 版本下的 todo.bat ，解决因路径导致的编译错误。

--- java/todo.bat
+++ java/todo.bat
@@ -1,7 +1,9 @@
 @echo off
 
-javac "%~dp0Todo.java" -encoding UTF-8 -d .
-java "%~dp0Todo" %1 %2 %3
+SET here=%~dp0
 
-REM javac "%~dp0Todo.java" -encoding UTF-8
-REM java -classpath "%~dp0" Todo %1 %2 %3
+del %here%Todo.class
+
+javac "%here%Todo.java" -encoding UTF-8 -d "%here:~0,-1%"
+
+java --class-path "%here:~0,-1%" Todo %1 %2 %3
