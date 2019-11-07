9f5b16632994faaf3b27934e6c716ccc34667d6e
ling <i@e7iling.com>
2019-11-08 00:33:45 +0800

    补充文字提示。

--- java/Todo.java
+++ java/Todo.java
@@ -1,9 +1,22 @@
-
+import java.util.Set;
 
 /**
  * Ｔｏｄｏ 类。
  */
 public class Todo {
+
+    /**
+     * 文字提示 Tips 。
+     */
+    private final String NO_CONTENT_ERR = "请填写任务内容。";
+    private final String TASK_NO_EXIST_ERR = "任务不存在。";
+    private final String NUM_ERR = "清输入合法的任务序号。";
+    private final String STEP_ERR = "清输入合法的移动步数。";
+    private final String ORIGIN_CONTENT_IS = "原内容是：";
+    private final String PLEASE_INPUT_CONTENT = "请输入要修改的内容：";
+    private final String YOUR_VERB_IS = "你的动作是： ";
+    private final String I_DONT_KNOW_WHAT_YOUR_WANT = "我不知道你想干什么~";
+    private final String ARE_YOU_SURE_CLEAR_ALL = "确认清空所有任务吗？[N/y]";
 
     /**
      * 数据库文件。
@@ -41,6 +54,16 @@ public class Todo {
      * @param args
      */
     public static void main(String[] args) {
+        Todo todo = new Todo();
+        todo.init();
+    }
+
+    public Todo() {
+
+    }
+
+    private void init() {
+        int[] myArr = new int[10];
 
     }
 }
