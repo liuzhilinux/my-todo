ca5a1d1be3abca831c9ca1d3b83bef6a4a90a27d
ling <i@e7iling.com>
2020-04-12 00:08:15 +0800

    补充初始化函数。

--- python/todo.py
+++ python/todo.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
 import sys
 
+# Ｔｏｄｏ 类。
 class Todo(object):
 
+    # 构造对象、初始化并调用指令。
     def __init__(self):
-        print('Hola Mundo!')
 
-        s1 = input('> ')
-        print(s1)
+        # 类似 JavaScript 的 parseInt 函数。
+        def parseInt(s):
+            try:
+                return re.match(r'^\d+', s).group(0)
+            except:
+                return 0
 
-        print(sys.argv)
-
-
-
+        args = sys.argv
+        
+        try:
+            self.__verb = args[1]
+        except:
+            self.__verb = None
+        try:
+            self.__n = parseInt(args[2])
+        except:
+            self.__n = None
+        try:
+            self.__content = args[2]
+        except:
+            self.__content = None
+        try:
+            self.__n1 = parseInt(args[3])
+        except:
+            self.__n1 = None
+        try:
+            self.__content1 = args[3]
+        except:
+            self.__content1 = None
 
 
 if __name__ == '__main__':
