65e79a522fdee3a250cb6bca2981e70e54de1d18
ling <i@e7iling.com>
2020-04-11 21:49:51 +0800

    初始化 Python 的 todo 模块。

---
+++ python/todo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    print('Hola Mundo!')
+
+    s1 = input('> ')
+    print(s1)
+
+    print(sys.argv)
+
