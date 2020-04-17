9fe9b07fd612f23321135001c5f0e1a03426753c
ling <i@e7iling.com>
2020-04-17 23:52:23 +0800

    修改方法定义的访问权限， 添加添加任务功能。

--- python/todo.py
+++ python/todo.py
@@ -59,39 +59,70 @@ class Todo(object):
             self.__content1 = None
 
         # 任务列表。
-        self.list = []
+        self.__list = []
         # 是否为无效动作。
-        self.noAction = False
+        self.__noAction = False
         # 命令行的输入参数是否验证通过，当输入有误的情况下，不打印列表，不保存列表。
-        self.validated = True
+        self.__validated = True
 
-        self.init()
+        self.__init()
 
-        self.exec()
+        self.__exec()
 
     # 销毁对象前打印任务列表并保存。
     def __del__(self):
-        pass
+        if self.__validated:
+            if self.__verb not in ['list', 'clearall']:
+                self.__save()
+            if self.__verb not in ['clearall']:
+                self.__display()
 
     # 初始化，获取数据库数据。
-    def init(self):
+    def __init(self):
         try:
-            with open(DB_PATH, 'r') as f:
-                self.list = json.load(f)
+            with open(DB_PATH, 'r', encoding='utf-8') as f:
+                self.__list = json.load(f)
         except FileNotFoundError:
-            with open(DB_PATH, 'w') as f:
+            with open(DB_PATH, 'w', encoding='utf-8') as f:
                 json.dump([], f)
         except json.decoder.JSONDecodeError:
-            self.list = []
+            self.__list = []
 
     # 保存数据到数据库。
-    def save(self):
-        with open(DB_PATH, 'w') as f:
-            json.dump(self.list, f)
+    def __save(self):
+        with open(DB_PATH, 'w', encoding='utf-8') as f:
+            json.dump(self.__list, f)
 
     # 执行操作。
-    def exec(self):
-        pass
+    def __exec(self):
+        verb = self.__verb
+
+        if verb == 'list':
+            pass
+        elif verb == 'add':
+            content = self.__content
+            if self.__checkContent(content):
+                self.__list.append({"content": content, "status": False})
+
+    # 显示任务列表。
+    def __display(self):
+        print('')
+
+        idxLen = len(str(len(self.__list)))
+        for idx, item in enumerate(self.__list):
+            idx = str(idx + 1)
+            space = ' ' * (idxLen - len(idx))
+            status = '[x]' if item['status'] else '[_]'
+            content = item['content']
+            print(space + idx + '.', status, content)
+
+    # 检查任务内容是否填写。
+    def __checkContent(self, content):
+        if not content:
+            self.__validated = False
+            print(TIPS['no_content_err'])
+            sys.exit()
+        return True
 
 
 if __name__ == '__main__':
